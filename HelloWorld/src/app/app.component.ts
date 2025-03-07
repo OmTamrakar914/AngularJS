74b6526f48b0d1ea33830582db256b685fca6f62
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-03-08 00:49:19 +0530

    displayed hello with input name using two data binding

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'HelloWorld';
-  imgUrl = "../../assets/logo1.jpg";
+  imgUrl = "assets/logo1.jpg";
+  url = "https://www.bridgelabz.com";
+  userName: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event: Event) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
--- HelloWorld/src/app/app.routes.ts
+++ HelloWorld/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+];
