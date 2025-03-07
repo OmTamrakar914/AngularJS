c24c752587205bdb5fba5780ec94819d724c5024
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-03-08 01:19:34 +0530

    [Om] Add. added username validation using input event binding

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   imgUrl = "assets/logo1.jpg";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
@@ -22,4 +23,15 @@ export class AppComponent {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+
+  onInput($event: any){
+    console.log("Change Event Occured!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}{a-zA-Z\\s}{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
+
 }
