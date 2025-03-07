ab501b8c0243be8afe4aa7be44a90be127aa32c2
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-03-08 01:35:08 +0530

    [Om] Add. Implemeted event binding launching website on clicking logo

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -12,8 +12,6 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/logo1.jpg";
   url = "https://www.bridgelabz.com";
-  userName: string = "";
-  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
@@ -22,16 +20,6 @@ export class AppComponent {
   onClick($event: Event) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
-  }
-
-  onInput($event: any){
-    console.log("Change Event Occured!", $event.data);
-    const nameRegex = RegExp('^[A-Z]{1}{a-zA-Z\\s}{2,}$');
-    if(nameRegex.test(this.userName)){
-      this.nameError = "";
-      return;
-    }
-    this.nameError = "Name is Incorrect!";
   }
 
 }
